081adfe8420ea8fa9cbf62745e91e414724d3c6c
Ubuntu <ubuntu@ip-172-31-27-61.ec2.internal>
2021-11-17 21:59:39 +0000

    added HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * The HelloWorld program displays "Hello, World!" whenever run.
+ *
+ * @author  Ryan Chung
+ * @version 1.0
+ * @since   2021-09-17
+ */
+
+public final class HelloWorld {
+
+    /**
+     * Prints "Hello, World!".
+     * */
+    private static void main() {
+
+        System.out.println("Hello, World!");
+    }
+
+}
+
